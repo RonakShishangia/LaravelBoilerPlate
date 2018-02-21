9a8393a63c7bd51cc7dbafaf967198d701484964
Ronak Shishangia <ronak.nkonn@gmail.com>
2018-02-21 18:58:22 +0530

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        /** Laravel seeder file call */
+        $this->call(LaratrustSeeder::class);
+    }
+}
